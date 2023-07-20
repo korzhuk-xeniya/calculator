cc677f341a2a0fe49bc95eb196cd36a9074eeccc
Ксения <korzhuk.xeniya@yandex.ru>
2023-07-20 13:08:44 +0300

    Task done

--- src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
@@ -1,26 +1,29 @@
 package pro.sky.calculator;
 
-public interface CalculatorService {
-    static String welcome() {
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService{
+    public static String welcome() {
         return "<b>Добро пожаловать в калькулятор</b>";
     }
 
-    static String plus(int num1, int num2) {
+    public static String plus(int num1, int num2) {
         int sum = num1 + num2;
         return num1 + " + " + num2 + " = " + sum;
     }
 
-    static String minus(int num1, int num2) {
+    public static String minus(int num1, int num2) {
         int difference = num1 - num2;
         return num1 + " - " + num2 + " = " + difference;
     }
 
-    static String multiply(int num1, int num2) {
+    public static String multiply(int num1, int num2) {
         int productOfMultiplication = num1 * num2;
         return num1 + " * " + num2 + " = " + productOfMultiplication;
     }
 
-    static String divide(int num1, int num2) {
+    public static String divide(int num1, int num2) {
         if (num2 == 0) {
             throw new RuntimeException("На ноль делить нельзя!");
         } else {
--- src/main/java/pro/sky/calculator/Controller.java
+++ src/main/java/pro/sky/calculator/Controller.java
@@ -1,16 +1,40 @@
 package pro.sky.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/calculator")
 public class Controller {
     private final CalculatorService calculatorService;
-    public Controller(CalculatorService calculatorService){
+
+    public Controller(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
     }
-    @GetMapping(path = "/calculator")
-    public String welcome () {
-        return CalculatorService.welcome();
+
+    @GetMapping
+    public String welcome() {
+        return CalculatorServiceImpl.welcome();
+    }
+
+        @GetMapping(path = "/plus")
+    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        String plus = CalculatorServiceImpl.plus(num1, num2);
+        return plus;
+    }
+
+    @GetMapping(path = "/minus")
+    public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return CalculatorServiceImpl.minus(num1, num2);
+    }
+@GetMapping(path = "/multiply")
+    public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return CalculatorServiceImpl.multiply(num1, num2);
+    }
+@GetMapping(path = "/divide")
+    public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return CalculatorServiceImpl.divide(num1, num2);
     }
 }
