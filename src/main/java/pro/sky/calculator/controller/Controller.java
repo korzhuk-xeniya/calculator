055ec2b408405090578e76a9059095defcdf09d9
Ксения <korzhuk.xeniya@yandex.ru>
2023-07-22 11:59:06 +0300

    add "На ноль делить нельзя"

--- src/main/java/pro/sky/calculator/controller/Controller.java
+++ src/main/java/pro/sky/calculator/controller/Controller.java
@@ -39,9 +39,8 @@ public class Controller {
     @GetMapping(path = "/divide")
     public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         if (num2 == 0) {
-            throw new RuntimeException("На ноль делить нельзя!");
-        } else {
-            return calculatorService.divide(num1, num2);
+            return ("На ноль делить нельзя!");
         }
+        return calculatorService.divide(num1, num2);
     }
 }
