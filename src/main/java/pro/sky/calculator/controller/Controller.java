3c255d2413fbcd0e5ac3bac823ed04befd9d7725
Ксения <korzhuk.xeniya@yandex.ru>
2023-07-22 11:54:37 +0300

    done methods non-static

--- src/main/java/pro/sky/calculator/controller/Controller.java
+++ src/main/java/pro/sky/calculator/controller/Controller.java
@@ -1,9 +1,11 @@
-package pro.sky.calculator;
+package pro.sky.calculator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.calculator.service.CalculatorService;
+import pro.sky.calculator.service.CalculatorServiceImpl;
 
 @RestController
 @RequestMapping(path = "/calculator")
@@ -16,27 +18,30 @@ public class Controller {
 
     @GetMapping
     public String welcome() {
-        return CalculatorServiceImpl.welcome();
+        return calculatorService.welcome();
     }
 
     @GetMapping(path = "/plus")
     public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        String plus = CalculatorServiceImpl.plus(num1, num2);
-        return plus;
+        return calculatorService.plus(num1, num2);
     }
 
     @GetMapping(path = "/minus")
     public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return CalculatorServiceImpl.minus(num1, num2);
+        return calculatorService.minus(num1, num2);
     }
 
     @GetMapping(path = "/multiply")
     public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return CalculatorServiceImpl.multiply(num1, num2);
+        return calculatorService.multiply(num1, num2);
     }
 
     @GetMapping(path = "/divide")
     public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return CalculatorServiceImpl.divide(num1, num2);
+        if (num2 == 0) {
+            throw new RuntimeException("На ноль делить нельзя!");
+        } else {
+            return calculatorService.divide(num1, num2);
+        }
     }
 }
--- src/main/java/pro/sky/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/calculator/service/CalculatorServiceImpl.java
@@ -1,34 +1,35 @@
-package pro.sky.calculator;
+package pro.sky.calculator.service;
 
 import org.springframework.stereotype.Service;
+import pro.sky.calculator.service.CalculatorService;
 
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
-    public static String welcome() {
+    public  String welcome() {
         return "<b>Добро пожаловать в калькулятор</b>";
     }
 
-    public static String plus(int num1, int num2) {
+    public  String plus(int num1, int num2) {
         int sum = num1 + num2;
         return num1 + " + " + num2 + " = " + sum;
     }
 
-    public static String minus(int num1, int num2) {
+    public  String minus(int num1, int num2) {
         int difference = num1 - num2;
         return num1 + " - " + num2 + " = " + difference;
     }
 
-    public static String multiply(int num1, int num2) {
+    public  String multiply(int num1, int num2) {
         int productOfMultiplication = num1 * num2;
         return num1 + " * " + num2 + " = " + productOfMultiplication;
     }
 
-    public static String divide(int num1, int num2) {
-        if (num2 == 0) {
-            throw new RuntimeException("На ноль делить нельзя!");
-        } else {
+    public  String divide(int num1, int num2) {
+//        if (num2 == 0) {
+//            throw new RuntimeException("На ноль делить нельзя!");
+//        } else {
             double quotientOfDivision = (double) num1 / num2;
             return num1 + " / " + num2 + " = " + quotientOfDivision;
         }
     }
-}
+
