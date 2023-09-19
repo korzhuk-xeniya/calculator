b6c0d1d92961e78f3ae5545383f6bf2699914551
Ксения <korzhuk.xeniya@yandex.ru>
2023-09-19 23:32:04 +0300

    tetst done

--- src/test/java/pro/sky/calculator/service/CalculatorServiceImplTest.java
+++ src/test/java/pro/sky/calculator/service/CalculatorServiceImplTest.java
@@ -14,26 +14,52 @@ public class CalculatorServiceImplTest {
     void plus_shouldReturnPositiveNumberWhenNum1AndNum2ArePositive(){
         long sum = calculatorService.plus(num1, num2);
 
-        assertEquals(num1 + num2, sum);
+        assertEquals((long) num1 + num2, sum);
     }
     @Test
-    void plus_shouldReturnNegstiveNumberWhenNum1IsNegativeAndNum2ArePositiveAndNum1BiggerThenNum2(){
+    void plus_shouldReturnNegativeNumberWhenNum1IsNegativeAndNum2ArePositiveAndNum1BiggerThenNum2(){
         int num1 = -3;
         long sum = calculatorService.plus(num1, num2);
 
-        assertEquals(num1 + num2, sum);
+        assertEquals((long) num1 + num2, sum);
     }
     @Test
     void minus_shouldReturnPositiveNumberWhenNum1AndNum2ArePositiveAndNum1BiggerThenNum2(){
         int num1 = 7;
         long difference = calculatorService.minus(num1, num2);
 
-        assertEquals(num1 - num2, difference);
+        assertEquals((long) num1 - num2, difference);
     }
     @Test
-    void minus_shouldReturnNegstiveNumberWhenNum1AndNum2ArePositiveAndNum2BiggerThenNum1(){
+    void minus_shouldReturnNegativeNumberWhenNum1AndNum2ArePositiveAndNum2BiggerThenNum1(){
         long difference = calculatorService.minus(num1, num2);
 
-        assertEquals(num1 - num2, difference);
+        assertEquals((long) num1 - num2, difference);
+    }
+    @Test
+    void multiply_shouldReturnPositiveNumberWhenNum1AndNum2ArePositive(){
+        long productOfMultiplication = calculatorService.multiply(num1, num2);
+
+        assertEquals((long) num1 * num2, productOfMultiplication);
+    }
+    @Test
+    void multiply_shouldReturnNegativeNumberWhenNum1IsNegativeAndNum2IsPositive(){
+        num1 = -4;
+        long productOfMultiplication = calculatorService.multiply(num1, num2);
+
+        assertEquals((long) num1 * num2, productOfMultiplication);
+    }
+    @Test
+    void divided_shouldReturnNegativeNumberWhenNum1IsNegativeAndNum2IsPositive(){
+        num1 = -6;
+        double quotientOfDivision = calculatorService.divide(num1, num2);
+
+        assertEquals((long) num1 / num2, quotientOfDivision);
+    }
+    @Test
+    void divided_shouldReturnPositiveNumberWhenNum1AndNum2ArePositive(){
+        double quotientOfDivision = calculatorService.divide(num1, num2);
+
+        assertEquals((double) num1 / num2, quotientOfDivision);
     }
 }
